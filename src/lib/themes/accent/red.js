0743c380d252ba84e1902aa7006761381a36ed6a
Titanium2099 <yasgh@protonmail.com>
2024-11-29 19:13:25 -0800

    changed colors to CT red

--- src/lib/themes/accent/red.js
+++ src/lib/themes/accent/red.js
@@ -1,9 +1,9 @@
 const guiColors = {
-    'motion-primary': '#ff4c4c',
-    'motion-primary-transparent': '#ff4c4ce6',
-    'motion-tertiary': '#cc3333',
+    'motion-primary': '#d0402e',
+    'motion-primary-transparent': '#d0402ee6',
+    'motion-tertiary': '#b82e1c',
 
-    'looks-secondary': '#ff4c4c',
+    'looks-secondary': '#d0402e',
     'looks-transparent': '#ff4d4d59',
     'looks-light-transparent': '#ff4d4d26',
     'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
@@ -13,12 +13,12 @@ const guiColors = {
     'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
     'extensions-light': 'hsla(10, 57%, 85%, 1)',
 
-    'drop-highlight': '#ff8c8c'
+    'drop-highlight': '#ff8c8c' 
 };
 
 const blockColors = {
-    checkboxActiveBackground: '#ff4c4c',
-    checkboxActiveBorder: '#cc3333'
+    checkboxActiveBackground: '#d0402e',
+    checkboxActiveBorder: '#b82e1c'
 };
 
 export {
