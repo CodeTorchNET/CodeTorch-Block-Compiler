31cc321f4b1509910e721ac4f9fc4ec0c1536d65
Titanium2099 <yasgh@protonmail.com>
2025-03-09 20:04:01 -0700

    can create a new project with ?new_project

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -4,12 +4,15 @@ import GUI from '../containers/gui.jsx';
 const searchParams = new URLSearchParams(location.search);
 const cloudHost = searchParams.get('cloud_host') || 'wss://clouddata.turbowarp.org';
 
+const creatingNewProject = new URLSearchParams(location.search).get('new_project') === '1';
+
 const RenderGUI = props => (
     <GUI
         cloudHost={cloudHost}
         canUseCloud
         hasCloudPermission
         canSave={true}
+        canCreateNew={creatingNewProject}
         basePath={process.env.ROOT}
         canEditTitle
         enableCommunity
