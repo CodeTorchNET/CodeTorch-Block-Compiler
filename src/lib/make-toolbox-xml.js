a7e3bf77f23ca8b0782018fc7898681c8288efde
Titanium2099 <yasgh@protonmail.com>
2025-03-18 11:14:28 -0700

    updated block name

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -748,7 +748,7 @@ const myBlocks = function (isInitialSetup, isStage, targetId, colors) {
 // eslint-disable-next-line max-len
 const extraTurboWarpBlocks = `
 <block type="argument_reporter_boolean"><field name="VALUE">is compiled?</field></block>
-<block type="argument_reporter_boolean"><field name="VALUE">is TurboWarp based?</field></block>
+<block type="argument_reporter_boolean"><field name="VALUE">is turbowarp based?</field></block>
 `;
 /* eslint-enable no-unused-vars */
 
