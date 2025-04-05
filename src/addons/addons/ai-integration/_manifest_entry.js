712e779ad8862a2087db89f534809555bf8405a0
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2025-04-04 22:37:20 -0400

    Switch to long password

--- src/addons/addons/ai-integration/_manifest_entry.js
+++ src/addons/addons/ai-integration/_manifest_entry.js
@@ -19,13 +19,13 @@ const manifest = {
     {
       "id": "GeminiAPIKey",
       "name": "Gemini API Key",
-      "type": "long_string",
+      "type": "long_password",
       "default": "",
     },
     {
       "id": "OpenRouterAPIKey",
       "name": "OpenRouter API Key",
-      "type": "long_string",
+      "type": "long_password",
       "default": "",
     }
   ],
