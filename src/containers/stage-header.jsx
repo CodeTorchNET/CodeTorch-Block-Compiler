479d0dee2b6fb1c606b68dfafc28bd65a2afcfe5
Titanium2099 <yasgh@protonmail.com>
2025-12-10 22:44:29 -0800

    modified full screen logic

--- src/containers/stage-header.jsx
+++ src/containers/stage-header.jsx
@@ -99,54 +99,24 @@ const mapDispatchToProps = dispatch => ({
     onSetStageSmall: () => dispatch(setStageSize(STAGE_SIZE_MODES.small)),
     onSetStageFull: () => dispatch(setStageSize(STAGE_SIZE_MODES.full)),
     onSetStageFullScreen: () => {
-        if (window.location.pathname === "/" || window.location.pathname.includes("/index.html") || window.location.pathname === "/build/") {
-            if (document.documentElement.requestFullscreen) {
-                document.documentElement.requestFullscreen().then(() => {
-                    dispatch(setFullScreen(true));
-                }).catch(err => {
-                    console.error('Failed to enter fullscreen:', err);
-                });
-            } else if (document.documentElement.webkitRequestFullscreen) { // Safari
-                document.documentElement.webkitRequestFullscreen();
-                dispatch(setFullScreen(true));
-            } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-                document.documentElement.mozRequestFullScreen();
-                dispatch(setFullScreen(true));
-            } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-                document.documentElement.msRequestFullscreen();
-                dispatch(setFullScreen(true));
-            } else {
-                console.warn('Fullscreen API is not supported in this browser.');
-            }
+        if (window.location.pathname === '/' ||
+            window.location.pathname.includes('/index.html') ||
+            window.location.pathname === '/build/') {
+            window.parent.postMessage({
+                type: 'block-compiler-action',
+                action: 'REQUEST_FULLSCREEN'
+            }, '*');
+            dispatch(setFullScreen(true));
         } else {
             dispatch(setFullScreen(true));
         }
     },
     onSetStageUnFullScreen: () => {
-        // Check if the document is currently in fullscreen
-        if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
-            if (document.exitFullscreen) {
-                document.exitFullscreen().then(() => {
-                    dispatch(setFullScreen(false));
-                }).catch(err => {
-                    console.error('Failed to exit fullscreen:', err);
-                });
-            } else if (document.webkitExitFullscreen) { // Safari
-                document.webkitExitFullscreen();
-                dispatch(setFullScreen(false));
-            } else if (document.mozCancelFullScreen) { // Firefox
-                document.mozCancelFullScreen();
-                dispatch(setFullScreen(false));
-            } else if (document.msExitFullscreen) { // IE/Edge
-                document.msExitFullscreen();
-                dispatch(setFullScreen(false));
-            } else {
-                console.warn('Fullscreen API is not supported in this browser.');
-            }
-        } else {
-            // Not in fullscreen, just dispatch the action
-            dispatch(setFullScreen(false));
-        }
+        window.parent.postMessage({
+            type: 'block-compiler-action',
+            action: 'CANCEL_FULLSCREEN'
+        }, '*');
+        dispatch(setFullScreen(false));
     },
     onOpenSettings: () => dispatch(openSettingsModal())
 });
