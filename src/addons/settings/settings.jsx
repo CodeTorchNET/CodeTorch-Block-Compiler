c5d0735bc5b2041ecba5f45931a1a94be6f0a42c
Miyo Sho <135030944+yuri-kiss@users.noreply.github.com>
2026-04-18 15:42:45 -0400

    chore: make this 2026

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2021-2025 Thomas Weber
+ * Copyright (C) 2021-2026 Thomas Weber
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
